7f2904f147f0802bc0162dbe06e2910eac67eb1c
parthjd <parthjd28@gmail.com>
2020-02-06 20:14:03 +0000

    created property function added

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -53,7 +53,6 @@ const addUser = function(user) {
       [user.name, user.email, user.password]
     )
     .then(res => {
-      console.log(res);
       return res.rows[0];
     });
 };
@@ -97,7 +96,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -132,7 +131,7 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // 5
-
+  console.log(queryString);
   // 6
   return pool.query(queryString, queryParams).then(res => res.rows);
 };
@@ -145,9 +144,62 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(
+      `
+    INSERT INTO properties (
+  owner_id,
+  title,
+  description,
+  thumbnail_photo_url,
+  cover_photo_url,
+  cost_per_night,
+  street,
+  city,
+  province,
+  post_code,
+  country,
+  parking_spaces,
+  number_of_bathrooms,
+  number_of_bedrooms 
+  )
+  values (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9,
+    $10,
+    $11,
+    $12,
+    $13,
+    $14
+  ) RETURNING *;
+    `,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms
+      ]
+    )
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    });
 };
 exports.addProperty = addProperty;
